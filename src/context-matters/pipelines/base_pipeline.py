46a2664cb2cfaa88712478d15332bd15c4967326
Fra-Tsuna <francescoarg@live.it>
2025-03-11 15:52:11 -0400

    major refactor completed

--- src/context-matters/pipelines/base_pipeline.py
+++ src/context-matters/pipelines/base_pipeline.py
@@ -1,6 +1,10 @@
 import os
-from typing import Dict, List
-class BasePipeline:
+import json
+from typing import Dict, List, Optional
+from abc import ABC, abstractmethod
+
+from utils import copy_file
+class BasePipeline(ABC):
     def __init__(self, **kwargs):
         self.base_dir: str  = kwargs["base_dir"]
         self.data_dir: str = kwargs["data_dir"]
@@ -9,6 +13,8 @@ class BasePipeline:
         self.generate_domain: bool = kwargs["generate_domain"]
         self.ground_in_sg: bool = kwargs["ground_in_sg"]
         self.model: str = kwargs["model"]
+        
+        self._name: str = "BasePipeline"
         
         self.api_key: str = os.getenv("API_KEY")
         
@@ -44,6 +50,92 @@ class BasePipeline:
             "other_2": 9,
             "pc_assembly": 3,
         }
+        
+        self.experiment_name: str = self._construct_experiment_name()
+        self.current_phase: Optional[str] = None
+    
+    @property
+    def name(self):
+        return self._name
+    
+    def _construct_experiment_name(self):
+        name = f"{self._name}_{self.model}"
+        if self.generate_domain:
+            name += "_gendomain"
+        if self.ground_in_sg:
+            name += "_ground"
+        return name
+    
+    
+    def _process_task(self, task_name, results_dir):
+        task_dir = os.path.join(self.data_dir, task_name)
+        os.makedirs(task_dir, exist_ok=True)
+
+        csv_filepath = os.path.join(results_dir, f"{task_name}.csv")
+        self._initialize_csv(csv_filepath)
+
+        task_file_path = os.path.join(self.data_dir, f"{task_name}.json")
+        with open(task_file_path) as f:
+            task_description = json.load(f)
+
+        domain_file_path, domain_description = self._setup_domain(task_description, task_name, task_dir, results_dir)
+
+        for scene_name in self.scenes_per_task[task_name]:
+            # Loop over the Gibson scenes of the task
+            self._process_scene(
+                task_name, scene_name, task_dir, results_dir,
+                domain_file_path, domain_description, csv_filepath
+            )
+
+    def _setup_domain(self, task_description, task_name, task_dir, results_dir):
+        # Set up domain files based on the pipeline configuration
+        if self.generate_domain:
+            return None, task_description["domain"]
+
+        domain_name = task_name.replace("_", "-") + ".pddl"
+        domain_file_path = os.path.join(task_dir, domain_name)
+        copy_file(domain_file_path, os.path.join(results_dir, task_name, "domain.pddl"))
+
+        return domain_file_path, None
+
+    def _process_scene(self, task_name, scene_name, task_dir, results_dir,
+                domain_file_path, domain_description, csv_filepath):
+
+        scene_dir = os.path.join(task_dir, scene_name)
+
+        for problem_id in range(1, self.problems_per_task[task_name] + 1):
+            # Loop over all the problems of the scene for that task
+            problem_dir = os.path.join(scene_dir, f"problem_{problem_id}")
+            results_problem_dir = os.path.join(results_dir, task_name, scene_name, f"problem_{problem_id}")
+            os.makedirs(results_problem_dir, exist_ok=True)
+            os.makedirs(os.path.join(results_problem_dir, "logs"), exist_ok=True)
+
+            scene_graph_file_path = os.path.join(problem_dir, f"{scene_name}.npz")
+
+            # Copy required files
+            for file_name in ["task.txt", "init_loc.txt", "description.txt"]:
+                copy_file(os.path.join(problem_dir, file_name), os.path.join(results_problem_dir, file_name))
+
+            self._run_and_log_pipeline(
+                task_name, scene_name, f"problem_{problem_id}", results_problem_dir,
+                domain_file_path, domain_description, scene_graph_file_path,
+                csv_filepath
+            )
 
     def run(self):
-        raise NotImplementedError("Method not implemented")
+        results_dir = os.path.join(self.results_dir, self.experiment_name)
+        os.makedirs(results_dir, exist_ok=True)
+        
+        for task_name in self.splits:
+        # Loop over all the tasks that we want to consider
+            self._process_task(task_name, results_dir)
+
+    @abstractmethod
+    def _run_and_log_pipeline(self, task_name, scene_name, problem_id, results_problem_dir,
+                            domain_file_path, domain_description, scene_graph_file_path,
+                            csv_filepath):
+        pass
+    
+    @abstractmethod
+    def _initialize_csv(self, csv_filepath):
+        pass
--- src/context-matters/pipelines/delta_pipeline.py
+++ src/context-matters/pipelines/delta_pipeline.py
@@ -1,71 +1,26 @@
 import os
-import json
 import csv
 import traceback
-from pathlib import Path
-from typing import Optional
+import json
 
 from .base_pipeline import BasePipeline
 from utils import (
-    copy_file, save_file, save_statistics,
-    read_graph_from_path,
-    get_verbose_scene_graph,
+    save_file, save_statistics,
+    save_graph
 )
 
 from logger_cfg import logging
 from planner import plan_with_output
-from pddl_generation import generate_domain, generate_problem, refine_problem, determine_problem_possibility
-from pddl_verification import translate_plan, VAL_validate, VAL_ground, verify_groundability_in_scene_graph
-from goal_relaxation import relax_goal, dict_replaceable_objects
+from pddlgym.core import PDDLEnv
+from pddl_verification import verify_groundability_in_scene_graph, convert_JSON_to_locations_dictionary, VAL_validate, VAL_parse, VAL_ground, translate_plan
+
+from delta_prompts import generate_pddl_domain, prune_scene_graph, generate_pddl_problem, decompose_pddl_goal
 
 class DeltaPipeline(BasePipeline):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._name: str = "DELTA"
-        
-        self.experiment_name: str = self._construct_experiment_name()
-        self.current_phase: Optional[str] = None
-        
-    @property
-    def name(self):
-        return self.name
-    
-    def _construct_experiment_name(self):
-        name = f"DELTA_{self.model}"
-        if self.generate_domain:
-            name += "_gendomain"
-        if self.ground_in_sg:
-            name += "_ground"
-        return name
-
-    def run(self):
-        results_dir = os.path.join(self.results_dir, self.experiment_name)
-        os.makedirs(results_dir, exist_ok=True)
-        
-        for task_name in self.splits:
-        # Loop over all the tasks that we want to consider
-            self._process_task(task_name, results_dir)
-        
-    def _process_task(self, task_name, results_dir):
-        task_dir = os.path.join(self.data_dir, task_name)
-        os.makedirs(task_dir, exist_ok=True)
-
-        csv_filepath = os.path.join(results_dir, f"{task_name}.csv")
-        self._initialize_csv(csv_filepath)
-
-        task_file_path = os.path.join(self.data_dir, f"{task_name}.json")
-        with open(task_file_path) as f:
-            task_description = json.load(f)
-
-        domain_file_path, domain_description = self._setup_domain(task_description, task_name, task_dir, results_dir)
-
-        for scene_name in self.scenes_per_task[task_name]:
-            # Loop over the Gibson scenes of the task
-            self._process_scene(
-                task_name, scene_name, task_dir, results_dir,
-                domain_file_path, domain_description, csv_filepath
-            )
 
     def _initialize_csv(self, csv_filepath):
         # Initialize CSV with headers
@@ -76,47 +31,11 @@ class DeltaPipeline(BasePipeline):
             writer = csv.writer(f, delimiter='|')
             writer.writerow(header)
             
-    def _setup_domain(self, task_description, task_name, task_dir, results_dir):
-    # Set up domain files based on the pipeline configuration
-        if self.generate_domain:
-            return None, task_description["domain"]
-
-        domain_name = task_name.replace("_", "-") + ".pddl"
-        domain_file_path = os.path.join(task_dir, domain_name)
-        copy_file(domain_file_path, os.path.join(results_dir, task_name, "domain.pddl"))
-
-        return domain_file_path, None
-    
-    
-    def _process_scene(self, task_name, scene_name, task_dir, results_dir,
-                    domain_file_path, domain_description, csv_filepath):
-    
-        scene_dir = os.path.join(task_dir, scene_name)
-
-        for problem_id in range(1, self.problems_per_task[task_name] + 1):
-            # Loop over all the problems of the scene for that task
-            problem_dir = os.path.join(scene_dir, f"problem_{problem_id}")
-            results_problem_dir = os.path.join(results_dir, task_name, scene_name, f"problem_{problem_id}")
-            os.makedirs(results_problem_dir, exist_ok=True)
-            os.makedirs(os.path.join(results_problem_dir, "logs"), exist_ok=True)
-
-            scene_graph_file_path = os.path.join(problem_dir, f"{scene_name}.npz")
-
-            # Copy required files
-            for file_name in ["task.txt", "init_loc.txt", "description.txt"]:
-                copy_file(os.path.join(problem_dir, file_name), os.path.join(results_problem_dir, file_name))
-
-            self._run_and_log_pipeline(
-                task_name, scene_name, f"problem_{problem_id}", results_problem_dir,
-                domain_file_path, domain_description, scene_graph_file_path,
-                csv_filepath
-            )
-            
     def _run_and_log_pipeline(self, task_name, scene_name, problem_id, results_problem_dir,
                             domain_file_path, domain_description, scene_graph_file_path,
                             csv_filepath):
         try:
-            results = self.delta_planning(
+            results = self._delta_planning(
                 goal_file_path=os.path.join(results_problem_dir, "task.txt"),
                 initial_location_file_path=os.path.join(results_problem_dir, "init_loc.txt"),
                 scene_graph_file_path=scene_graph_file_path,
@@ -188,7 +107,7 @@ class DeltaPipeline(BasePipeline):
         return
     
     
-    def delta_planning(self, **kwargs):
+    def _delta_planning(self, **kwargs):
         
         goal_file_path = kwargs["goal_file_path"]
         initial_location_file_path = kwargs["initial_location_file_path"]
@@ -196,3 +115,273 @@ class DeltaPipeline(BasePipeline):
         domain_file_path = kwargs["domain_file_path"]
         results_dir = kwargs["results_dir"]
         domain_description = kwargs["domain_description"]
+        
+        logs_dir = os.path.join(results_dir, "logs")
+        os.makedirs(logs_dir, exist_ok=True)
+        
+        initial_robot_location = open(initial_location_file_path, "r").read()
+        
+        if domain_file_path is None:
+            self.current_phase = "DOMAIN GENERATION"
+            assert (
+                domain_description is not None,
+                "Provide the domain or the domain description"
+            )
+            
+            logging.info("Generating PDDL domain")
+            domain_pddl = generate_pddl_domain(goal_file_path, domain_description,
+                                                logs_dir=logs_dir, model=self.model)
+            domain_file_path = os.path.join(results_dir, "domain", "domain.pddl")
+            
+            # Save generated PDDL domain
+            os.makedirs(os.path.dirname(domain_file_path), exist_ok=True)
+            with open(domain_file_path, "w") as f:
+                f.write(domain_pddl)
+                
+        self.current_phase = "DOMAIN VALIDATION"
+        logging.info("Validating the domain")
+        val_parse_success, val_parse_log = VAL_validate(domain_file_path)
+        save_statistics(
+            dir=results_dir,
+            workflow_iteration=0,
+            phase=self.current_phase,
+            VAL_validation_log=val_parse_log
+        )
+        
+        if not val_parse_success:
+            return domain_file_path, None, None, None, False, False, None, "DOMAIN_GENERATION", val_parse_log
+        logging.info("Domain validation successful")
+        
+        logging.info("Pruning scene graph")
+        self.current_phase = "PRUNING_SCENE_GRAPH"
+        pruned_sg = prune_scene_graph(scene_graph_file_path, goal_file_path, initial_robot_location, 
+                                        logs_dir=logs_dir, model=self.model)
+        
+        # Save pruned scene graph
+        pruned_sg_path_readable = os.path.join(results_dir, "pruned_sg.txt")
+        with open(pruned_sg_path_readable, "w") as f:
+            f.write(json.dumps(pruned_sg, indent=4))
+
+        pruned_sg_path_npz = os.path.join(results_dir, "pruned_sg.npz")
+        save_graph(pruned_sg, pruned_sg_path_npz)
+        
+        logging.info("Generating PDDL problem")
+        self.current_phase = "PROBLEM GENERATION"
+        problem_pddl = generate_pddl_problem(pruned_sg_path_npz, goal_file_path, domain_file_path,
+                                            initial_robot_location, logs_dir=logs_dir, model=self.model)
+        
+        # Save the generated PDDL problem
+        problem_pddl_path = os.path.join(results_dir, "problem.pddl")
+        with open(problem_pddl_path, "w") as f:
+            f.write(problem_pddl)
+            
+        logging.info("Validating the problem")
+        val_parse_success, val_parse_log = VAL_validate(domain_file_path, problem_pddl_path)
+        
+        save_statistics(
+            dir=results_dir,
+            workflow_iteration=0,
+            phase=self.current_phase,
+            VAL_validation_log=val_parse_log
+        )
+        
+        if not val_parse_success:
+            return domain_file_path, pruned_sg, problem_pddl_path, None, False, False, None, "PROBLEM_GENERATION", val_parse_log
+        logging.info("Problem validation successful")
+        
+        logging.info("Generating PDDL sub-goals")
+        self.current_phase = "SUBGOAL_GENERATION"
+        sub_goals_pddl = decompose_pddl_goal(problem_pddl_path, domain_file_path, initial_robot_location, 
+                                            logs_dir=logs_dir, model=self.model)
+        
+        logging.info("Autoregressive planning and grounding")
+        self.current_phase="PLANNING"
+        
+        old_pddl_env = None
+        plans = []
+        sub_goals_file_paths = []
+        
+        for i, sub_goal in enumerate(sub_goals_pddl):
+            
+            sub_goal_dir = os.path.join(results_dir, f"sub_goal_{i+1}")
+            os.makedirs(sub_goal_dir, exist_ok=True)
+            sub_goal_file = os.path.join(sub_goal_dir, f"sub_goal_{i+1}.pddl")
+            sub_goals_file_paths.append(sub_goal_file)
+            with open(sub_goal_file, "w") as f:
+                f.write(sub_goal)
+                
+        # Evaluate the sequence of sub-goals
+        # 1) Replace the :object and :init sections of each subgoal with the goal state of the prev subgoal (or the initial state of the non-decomposed problem)
+        # 2) Generate a plan
+        # 3) [OPTIONAL] Ground the plan in the scene graph
+        for i, sub_goal in enumerate(sub_goals_pddl):
+            self.current_phase = f"SUBGOAL_{i+1}:GENERATION"
+
+            grounding_succesful = False
+            planning_succesful = False
+
+            sub_goal_dir = os.path.join(results_dir, f"sub_goal_{i+1}")
+            sub_goal_file = os.path.join(sub_goal_dir, f"sub_goal_{i+1}.pddl")
+
+            # If this is the first iteration (old_pddl_env is None), use the PDDLEnv of the main (non-decomposed problem) to get the initial state of the first subgoal
+            if old_pddl_env is None:
+                logging.debug(domain_file_path)
+                logging.debug(results_dir)
+                old_pddl_env = PDDLEnv(domain_file_path, results_dir, operators_as_actions=True)
+                old_pddl_env.reset()
+
+            # Get the final state of the previous environment
+            obs = old_pddl_env.get_state()
+
+            # Extract PDDL objects
+            pddl_objects = []
+            pddl_objects_str = "(:objects\n"
+            for obj in obs.objects:
+                obj = str(obj).split(":")
+                pddl_objects.append((obj[0], obj[1]))
+                pddl_objects_str += f"    {obj[0]} - {obj[1]}\n"
+            pddl_objects_str += ")\n\n"
+
+            # Extract PDDL predicates        
+            state = obs.literals
+
+            pddl_predicates = []
+            pddl_predicates_str = "(:init\n"
+            for literal in state:
+                predicate_name = literal.predicate.name
+                predicate_variables = []
+                for variable in literal.variables:
+                    predicate_variables.append(variable.split(":")[0])
+                pddl_predicates_str += f"    ({predicate_name} {' '.join(predicate_variables)})\n"
+                pddl_predicates.append((predicate_name, *predicate_variables))
+            pddl_predicates_str += ")\n\n"
+
+            # In the sub-goal problem file, replace the :objects and :init sections with the goal state of the previous sub-goal
+            sub_goal_pddl = open(sub_goal_file, "r").read()
+            
+            # Replace the whole of the :objects and :init sections of the sub-goal with the goal state of the previous sub-goal
+            sub_goal_pddl = sub_goal_pddl.replace(sub_goal_pddl[sub_goal_pddl.index("(:objects"):sub_goal_pddl.index("(:init")], pddl_objects_str)
+            sub_goal_pddl = sub_goal_pddl.replace(sub_goal_pddl[sub_goal_pddl.index("(:init"):sub_goal_pddl.index("(:goal")], pddl_predicates_str)
+
+            # Save the modified sub-goal
+            with open(sub_goal_file, "w") as f:
+                f.write(sub_goal_pddl)
+
+            self.current_phase = f"SUBGOAL_{i+1}:PLANNING"
+
+            # Initialize env
+            new_pddl_env = PDDLEnv(domain_file_path, sub_goal_dir, operators_as_actions=True)
+
+            output_plan_file_path = os.path.join(sub_goal_dir, f"plan_{i+1}.txt")
+
+            # Compute plan on env
+            plan, pddlenv_error_log, planner_error_log, planner_statistics = plan_with_output(domain_file_path, sub_goal_dir, output_plan_file_path, env=new_pddl_env)
+            
+            if plan is not None and plan:       
+                
+                planning_succesful = True
+                
+                # Verify with VAL
+                translated_plan_path = os.path.join(sub_goal_dir, f"translated_plan_{i+1}.txt")
+
+                # Translate the plan into a format parsable by VAL
+                translate_plan(output_plan_file_path, translated_plan_path)
+
+                logging.info("Validating the plan")
+                self.current_phase = f"SUBGOAL_{i+1}:VAL:VALIDATION"
+                
+                # Use VAL to validate the plan
+                val_succesful, val_log = VAL_validate(domain_file_path, sub_goal_file, translated_plan_path)
+                logging.info(f"Result: {val_succesful}")
+                logging.debug(val_log)
+
+                logging.info("Validating grounding")
+                # Use VAL to ground the plan
+                val_ground_succesful, val_ground_log = VAL_ground(domain_file_path, sub_goal_file)
+                logging.info(f"Result: {val_ground_succesful}")
+                logging.debug(val_ground_log)
+
+                grounding_succesful = val_succesful and val_ground_succesful
+
+                self.current_phase = f"SUBGOAL_{i+1}:VAL:GROUNDING"
+                save_statistics(
+                    dir=results_dir,
+                    workflow_iteration=0,
+                    phase=self.current_phase,
+                    plan_succesful=planning_succesful,
+                    pddlenv_error_log=pddlenv_error_log,
+                    planner_error_log=planner_error_log,
+                    VAL_validation_log=val_log,
+                    VAL_grounding_log=val_ground_log,
+                    planner_statistics=planner_statistics
+                )
+
+
+                # If this experiment requires it, try grounding the plan in the real scene graph
+                if grounding_succesful and self.ground_in_sg:
+                    self.current_phase = f"SUBGOAL_{i+1}:SCENE_GRAPH:GROUNDING"
+                    logging.info("Grounding in scene graph")
+
+                    # Convert the scene graph into a format readable by the grounder
+                    extracted_locations_dictionary = convert_JSON_to_locations_dictionary(pruned_sg)
+
+                    # Save the extracted locations dictionary to file
+                    extracted_locations_dictionary_file_path = os.path.join(sub_goal_dir, "extracted_locations_dictionary.json")
+                    with open(extracted_locations_dictionary_file_path, "w") as f:
+                        json.dump(extracted_locations_dictionary, f, indent=4)
+
+                    grounding_success_percentage, grounding_error_log = verify_groundability_in_scene_graph(
+                        plan, 
+                        None, 
+                        domain_file_path=domain_file_path, 
+                        problem_dir=sub_goal_dir, 
+                        move_action_str="move_to",
+                        location_relation_str="at",
+                        location_type_str="room",
+                        initial_robot_location=initial_robot_location,
+                        pddlgym_environment = new_pddl_env,
+                        locations_dictionary = extracted_locations_dictionary
+                    )
+                    
+                    logging.info(f"Result: {grounding_success_percentage}")
+                    logging.debug(grounding_error_log)
+
+                    grounding_succesful = grounding_success_percentage == 1
+
+                    save_statistics(
+                        dir=results_dir,
+                        workflow_iteration=0,
+                        phase=self.current_phase,
+                        plan_succesful=planning_succesful,
+                        pddlenv_error_log=pddlenv_error_log,
+                        planner_error_log=planner_error_log,
+                        VAL_validation_log=val_log,
+                        VAL_grounding_log=val_ground_log,
+                        grounding_success_percentage=grounding_success_percentage,
+                        scene_graph_grounding_log=grounding_error_log
+                    )
+
+                if not grounding_succesful:
+                    plans.append(plan)
+                    return domain_file_path, pruned_sg, problem_pddl_path, sub_goals_file_paths, True, False, plans, self.current_phase, grounding_error_log
+                
+            else:
+                error_log = pddlenv_error_log if pddlenv_error_log is not None else planner_error_log
+                
+                save_statistics(
+                    dir=results_dir,
+                    workflow_iteration=0,
+                    phase=self.current_phase,
+                    plan_succesful=planning_succesful,
+                    pddlenv_error_log=pddlenv_error_log,
+                    planner_error_log=planner_error_log
+                )
+                
+                return domain_file_path, pruned_sg, problem_pddl_path, sub_goals_file_paths, False, False, plans, f"PLANNING:SUBGOAL_{i}", error_log
+                    
+            plans.append(plan)
+            new_pddl_env = old_pddl_env
+            logging.info(f"Sub-goal {i+1} completed successfully")
+            
+        return domain_file_path, pruned_sg, problem_pddl_path, sub_goals_file_paths, planning_succesful, grounding_succesful, plans, "", ""
+            
